2331cd319e9ad7b1064d5a41271a73690e9c9ff2
Vitalii Rybalko <vilko.a@gmail.com>
2018-04-17 01:54:59 +0300

    add iptables config rule

--- start_vm.sh
+++ start_vm.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# iptables -t nat -A OUTPUT -p tcp --destination 10.0.2.15/31 -j DNAT --to-destination 127.0.0.1:10080
 qemu-system-i386 -cdrom ~/Downloads/Darkly_i386.iso -m 2G -hda gopa.img -net user,hostfwd=tcp::10080-:80 -net nic
